e4cb0b6a2713c51e2e74189fd9662b108067c4b5
Arturo De la Garza <agzertuche@gmail.com>
2019-10-14 00:30:54 -0500

    Updates

---
+++ precache-manifest.099c9412b181503a54b5dc2a5b9dea74.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9408244330029538d7d6830b833df30",
+    "url": "/minesweeper/index.html"
+  },
+  {
+    "revision": "30cf5a9a242e81a2cdbc",
+    "url": "/minesweeper/static/css/main.aa83045b.chunk.css"
+  },
+  {
+    "revision": "91ed2c83612c76334825",
+    "url": "/minesweeper/static/js/2.e49d4db9.chunk.js"
+  },
+  {
+    "revision": "30cf5a9a242e81a2cdbc",
+    "url": "/minesweeper/static/js/main.d8c997fe.chunk.js"
+  },
+  {
+    "revision": "1977715552b944737d91",
+    "url": "/minesweeper/static/js/runtime-main.43eeeb0c.js"
+  }
+]);
